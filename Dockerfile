a731a66cd30e87e24a3d895d86a5d77d6e8833db
Mark Leybourne <Mark.Leybourne@interact-intranet.com>
2018-09-20 11:30:18 +0100

    Basic working example

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3
+FROM python:2.7-slim
 ADD pyserve.py /
+RUN pip install --trusted-host pypi.python.org Flask
 EXPOSE 8083
 CMD ["python", "./pyserve.py"]
--- pyserve.py
+++ pyserve.py
@@ -1,26 +1,13 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
+from flask import Flask
+import os
+import socket
 
-class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+app = Flask(__name__)
+@app.route("/")
 
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
-        
-def run(server_class = HTTPServer, handler_class = S, port = 8083):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print('Starting httpd...')
-    httpd.serve_forever()
+def hello():
+    html = "Hello from a Python service on host {hostname}"
+    return html.format(hostname=socket.gethostname())
 
 if __name__ == "__main__":
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+    app.run(host='0.0.0.0', port=4000)
